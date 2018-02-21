61584fa1af9ed7574fd8e91af4c4abcb97b62b9a
Nadia Manzari <nadiamanzari@Nadias-MacBook-Air.local>
2018-02-21 15:22:03 +0100

    CreateDocscontrollers

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,2 +1,48 @@
 class DocsController < ApplicationController
+	before_action :find_doc, only: [:show, :edit, :update, :destroy]
+     
+ 	def index
+        @docs = Doc.all(“created_at_DESC”)
+ 	end
+
+  def show
+      #...
+  end
+
+  def edit
+        #...
+  end
+
+  def update
+        #...
+  end
+
+  def destroy
+        #...
+  end
+
+  def new
+    @doc = Doc.new
+  end
+
+  def create
+    @doc = Doc.new(doc_params)
+    if @doc.save
+      redirect_to @doc
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def find_doc
+		@doc = Doc.find(params[:id])
+	end
+
+  def doc_params
+    params.require(:doc).permit(:title, :content)
+  end
+
 end
+
